a0d857b92f7ea169448b97741d865f9827cf935c
Valera Leontyev <feedbee@gmail.com>
2024-01-07 14:16:02 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,34 @@
+# AI Read It Web API
+
+## Description
+
+This project is a Web API that uses AI to read.
+
+## Installation
+
+```bash
+# Clone the repository
+git clone https://github.com/feedbee/ai-read-it-web-api.git
+
+# Navigate into the directory
+cd ai-read-it-web-api
+
+# Install dependencies
+npm install
+```
+
+# Usage
+
+```bash
+# Start the server
+npm start
+```
+
+# API Endpoints
+
+GET /api/v1/read: This endpoint does...
+Contributing
+Provide instructions on how to contribute to your project.
+
+License
+This project is licensed under the MIT License - see the LICENSE.txt file for details.
---
+++ index.js
@@ -0,0 +1,61 @@
+const express = require('express');
+const { Readable } = require('stream');
+const bodyParser = require('body-parser');
+const app = express();
+const port = 3001;
+
+app.use(bodyParser.json()); // for parsing application/json
+
+// CORS middleware
+const allowCrossDomain = function(req, res, next) {
+  const allowedOrigins = ['http://localhost:3000'];
+
+  const origin = req.headers.origin;
+  if (allowedOrigins.includes(origin)) {
+       res.setHeader('Access-Control-Allow-Origin', origin);
+  }
+
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+
+  next();
+}
+app.use(allowCrossDomain);
+
+const aiReadIt = require('ai-read-it');
+aiReadIt.init(process.env.OPENAI_API_KEY);
+
+app.post('/tts', (req, res) => {
+  const textToConvert = req.body.text;
+
+  if (textToConvert === undefined) {
+    res.status(400).json({ error: "Text was not provided. Please, send POST body in JSON with 'text' filed that contains text to read." });
+    return;
+  }
+
+  try {
+    const readable = Readable.from(aiReadIt.largeTextToSpeech(textToConvert, {chunkSize: 2000}));
+    res.type('audio/mpeg');
+    readable.pipe(res);
+  } catch (error) {
+    console.error(error);
+    res.type('text/json');
+    res.status(500).json({ "error": error.message });
+  }
+
+  // aiReadIt.smallTextToSpeech(textToConvert)
+  //   .then(audioBuffer => {
+  //     res.type('audio/mpeg');
+  //     res.send(audioBuffer);
+  //   })
+  //   .catch(error => {
+  //     console.error("Error:", error);
+  //     res.type('text/json');
+  //     res.status(500).json({ error });
+  //   });
+});
+
+// Start the server
+app.listen(port, () => {
+  console.log(`Server started on port ${port}`);
+});
